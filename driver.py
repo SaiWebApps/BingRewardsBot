98149728f7efa47cbb4f7cada34c55bc504cdc6d
Sairam Krishnan <sairambkrishnan@gmail.com>
2015-12-11 00:55:03 -0800

    Updated driver.py to use credentialsprocessor.
    Also refactored to invoke multithreaded bot management.
    Still need to test.

--- driver.py
+++ driver.py
@@ -1,17 +1,21 @@
-from bingrewardsaccount import AccountCredentials
-from bingrewardsbot import DesktopBingRewardsBot, MobileBingRewardsBot
+import argparse
+
+from bingrewardsbot import BotConfig, BingRewardsBotManager
 from browser.webdrivermanager.browsertypes import BrowserType
+from credentialsprocessor import process_credentials
 
 def main():
-    # Enter Hotmail/Outlook creds here.
-    creds = []
-    desktop_bot = DesktopBingRewardsBot(BrowserType.Chrome)
-    desktop_bot.execute(creds, 30)
-    desktop_bot.finish()
+	# Extract target Bing Rewards accounts' credentials from the specified JSON file.
+	parser = argparse.ArgumentParser(description = 'Meet daily Bing Rewards desktop and mobile search quota.')
+	parser.add_argument('-f', '--filename', required = True, help = 'Name of JSON file with Bing Rewards account credentials')
+	args = parser.parse_args()	
+	creds = credentials_processor.process_credentials(args.filename)
 
-    mobile_bot = MobileBingRewardsBot(BrowserType.Chrome)
-    mobile_bot.execute(creds, 20)
-    mobile_bot.finish()
+	# Perform searches.
+	desktop_bot_config = BotConfig(BrowserType.PhantomJS, 30)
+	mobile_bot_config = BotConfig(BrowserType.PhantomJS, 20)
+	mgr = BingRewardsBotManager(desktop_bot_config, mobile_bot_config)
+	mgr.execute(creds, creds)
 
 if __name__ == '__main__':
     main()
